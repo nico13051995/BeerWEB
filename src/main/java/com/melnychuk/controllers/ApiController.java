f64766f5abc6942663614374882eeda38cff4bb2
iKethavel <blackswordgc@gmail.com>
2017-04-02 00:32:10 +0300

    try fix mobile error [3]

--- src/main/java/com/melnychuk/controllers/ApiController.java
+++ src/main/java/com/melnychuk/controllers/ApiController.java
@@ -8,10 +8,12 @@ import com.melnychuk.entities.SalePoint;
 import com.melnychuk.managers.SalePointManager;
 import com.melnychuk.objects.PointsHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -31,8 +33,9 @@ public class ApiController
     @ResponseBody
     public String welcome(HttpServletRequest request) throws IOException
     {
-        File file = new File("E:\\WebProjects\\javaProjects\\BeerMap\\src\\main\\webapp\\WEB-INF\\resources\\GeoLiteCity.dat");
-        LookupService cl = new LookupService(file.getAbsolutePath(), LookupService.GEOIP_MEMORY_CACHE | LookupService.GEOIP_CHECK_CACHE);
+        String path = request.getSession().getServletContext().getRealPath("/WEB-INF/classes/GeoLiteCity.dat");
+        System.out.println(request.getSession().getServletContext().getRealPath("/WEB-INF/classes/GeoLiteCity.dat"));
+        LookupService cl = new LookupService(path.toString(), LookupService.GEOIP_MEMORY_CACHE | LookupService.GEOIP_CHECK_CACHE);
         String ipAddress = request.getRemoteAddr();
         Location location = cl.getLocation(ipAddress);
         if (location == null) location = cl.getLocation("95.69.203.155");
